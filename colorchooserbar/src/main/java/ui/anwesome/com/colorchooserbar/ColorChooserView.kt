eda1d74387b0cf99875a5e59c4fc17aafc40c41e
Anwesh43 <anweshthecool0@gmail.com>
2017-12-12 00:52:04 +0530

    adding ColorChooserBar

--- colorchooserbar/src/main/java/ui/anwesome/com/colorchooserbar/ColorChooserView.kt
+++ colorchooserbar/src/main/java/ui/anwesome/com/colorchooserbar/ColorChooserView.kt
@@ -3,3 +3,24 @@ package ui.anwesome.com.colorchooserbar
 /**
  * Created by anweshmishra on 12/12/17.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+import android.util.AttributeSet
+
+class ColorChooserView:View {
+    constructor(ctx:Context):super(ctx)
+    constructor(ctx: Context,attrs:AttributeSet):super(ctx,attrs)
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas: Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
