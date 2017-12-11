12e5d2f60e11b0bdacd7812773522d7fbb3aaa6b
Anwesh43 <anweshthecool0@gmail.com>
2017-12-12 01:14:25 +0530

    adding ColorChooserContainer

--- colorchooserbar/src/main/java/ui/anwesome/com/colorchooserbar/ColorChooserView.kt
+++ colorchooserbar/src/main/java/ui/anwesome/com/colorchooserbar/ColorChooserView.kt
@@ -7,6 +7,8 @@ import android.view.*
 import android.content.*
 import android.graphics.*
 import android.util.AttributeSet
+import java.util.concurrent.ConcurrentLinkedQueue
+
 val colors:Array<String> = arrayOf("#673AB7","#448AFF","#FF5722","#f44336","#1A237E","#AD1457")
 class ColorChooserView:View {
     constructor(ctx:Context):super(ctx)
@@ -23,7 +25,7 @@ class ColorChooserView:View {
         }
         return true
     }
-    data class ColorChooserCircle(var i:Int,var x:Float,var y:Float,var r:Float,var or:Float = r) {
+    data class ColorChooserCircle(var i:Int,var x:Float,var y:Float,var r:Float,var or:Float = r):Comparable<ColorChooserCircle> {
         fun draw(canvas:Canvas,paint:Paint) {
             paint.color = Color.parseColor(colors[i])
             canvas.drawCircle(x,y,r,paint)
@@ -35,5 +37,44 @@ class ColorChooserView:View {
             r = or+(mr-or)*scale
         }
         fun handleTap(x:Float,y:Float):Boolean = x>=this.x -r && x<=this.x+r && y<=this.y -r && y>=this.y+r
+        override fun compareTo(other:ColorChooserCircle):Int = (x-other.x).toInt()
     }
+    data class ColorChooserContainer(var w:Float,var h:Float,var gap:Float = Math.min(w/(2* colors.size+1),h/2)) {
+        var circles:ConcurrentLinkedQueue<ColorChooserCircle> = ConcurrentLinkedQueue()
+        var prev:ColorChooserCircle?=null
+        var curr:ColorChooserCircle?=null
+        init {
+            if(colors.size > 0) {
+                var x = 3*gap/2
+                for (i in 0..colors.size - 1) {
+                    circles.add(ColorChooserCircle(i,x,h/2,gap/4))
+                    x+=2*gap
+                }
+            }
+        }
+        fun draw(canvas: Canvas,paint:Paint) {
+            circles.forEach { circle ->
+                circle.draw(canvas,paint)
+            }
+        }
+        fun update(stopcb:()->Unit) {
+
+        }
+        fun handleTap(x:Float,y:Float,startcb:()->Unit) {
+            circles.forEach { circle ->
+                circle.handleTap(x,y)
+                startcb()
+            }
+        }
+    }
+}
+fun ConcurrentLinkedQueue<ColorChooserView.ColorChooserCircle>.at(j:Int):ColorChooserView.ColorChooserCircle? {
+    var i = 0
+    forEach {
+        if(i == j) {
+            return it
+        }
+        i++
+    }
+    return null
 }
